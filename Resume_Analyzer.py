1833cda48ef6a3caf823b39fa351b56e5123fe78
Nishant Chauhan <166284550+Clipperai@users.noreply.github.com>
2026-05-08 13:06:38 +0530

    Refactor Resume Analyzer for improved functionality

--- Resume_Analyzer.py
+++ Resume_Analyzer.py
@@ -39,7 +39,7 @@ if 'history' not in st.session_state:
 st.title("Resume Analyzer AI")
 
 file = st.file_uploader("Upload a file", type=['pdf', 'docs'])
-role = st.selectbox('Select', ['-Select-', 'Frontend Dev', 'Backend Dev', 'Full Stack Dev', 'ML Dev', 'Data Analyst', 'AI Engineer'])
+role = st.selectbox('Job Description', ['-Select-', 'Frontend Dev', 'Backend Dev', 'Full Stack Dev', 'ML Dev', 'Data Analyst', 'AI Engineer'])
 button = st.button("Submit")
 
 if file and button:
