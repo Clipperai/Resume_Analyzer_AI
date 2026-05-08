9705476cc88c90f1df1ba6b21961d05676560cad
Nishant Chauhan <166284550+Clipperai@users.noreply.github.com>
2026-05-08 12:54:28 +0530

    Refactor Resume Analyzer code structure

--- Resume_Analyzer.py
+++ Resume_Analyzer.py
@@ -53,12 +53,12 @@ if file:
                 Resume:
                 {resume_text}
                 """
-                result = ask_ai(resume_text)
-                st.subheader("Analysis Result")
-                st.write(result)
-                st.session_state.history.append({
-        "name": file.name,
-        "content": result,
+                    result = ask_ai(resume_text)
+                    st.subheader("Analysis Result")
+                    st.write(result)
+                    st.session_state.history.append({
+            "name": file.name,
+            "content": result,
 })
 
 
